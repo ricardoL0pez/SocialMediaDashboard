786afb48b6a849857da7c992da0dd72ed8b0bcca
nitidoart <113176158+nitidoart@users.noreply.github.com>
2024-04-02 12:07:56 +0200

    modified(App.jsx):creacion de estados

--- vite-tailwind/src/App.jsx
+++ vite-tailwind/src/App.jsx
@@ -1,69 +1,80 @@
 import './App.css';
-import uno from './assets/images/uno.png';
-import dos from './assets/images/dos.png';
-import tres from './assets/images/tres.png';
-import cuatro from './assets/images/cuatro.png';
-import cinco from './assets/images/cinco.png';
-import seis from './assets/images/seis.png';
+import React, {useState} from 'react';
 import galaga from './assets/images/galaga.jpeg';
 
 
 function App() {
+const [notification, setNotification] = useState(4)
+const [notRead, setNotRead] = useState(true)
 
+const handlePostClick = (event) => {
+  const post = event.currentTarget;
+  const status = post.querySelector('.status');
+  status.classList.remove('not-read');
+  setNotification((prevNotification) => Math.max(0, prevNotification - 1));
+  if (notification === 1) {
+    setNotRead(false);
+  }
+};
+
+const handleMarkAllRead = () => {
+  setNotification(0);
+  setNotRead(false);
+}
   return (
-    <body>
-      {/* Frontend Mentor | Notifications page */}
+    <>
+      {/* Notifications page */}
       <section className='main'>
         <div className='notifications'>
-          <h3 className='title'>Notifications <span className='number'>4</span></h3>
-          <p className='mark'>Mark all as read</p>
+          <h3 className='title'>Notifiche <span className='number'>{notification}</span></h3>
+          <p className={`mark ${!notRead ? 'markAllRead':''}` } onClick={handleMarkAllRead}>Contrassegnare tutto come letto</p>
         </div>
 
-        <div className='post'>
-          <img className='image' src={uno} alt="" />
+        <div className='post' onClick={handlePostClick}>
+          <div className='image1'></div>
           <div>
             <p>
-              <span className='name'>Chica </span>
+              <span className='name'>Chicha </span>
               <span>reacted to your recent post </span>
-              <span className='reaction'>My first tournament today!</span>
-              <span className='status not-read'></span>
+              <span className='reaction'>Il mio primo torneo oggi!</span>
+              <span className={`status ${notRead ? 'not-read' : ''}`}> </span>
             </p>
             <p className='time'>1m fa</p>
           </div>
         </div>
 
-        <div className='post'>
-          <img className='image' src={dos} alt="" />
+        <div className='post' onClick={handlePostClick}>
+        <div className='image2'></div>
           <div>
             <p>
               <span className='name'>Chontaduro </span>
-              <span>followed you</span>
-              <span className='status not-read'></span>
+              <span>ti ho seguito</span>
+              <span className={`status ${notRead ? 'not-read' : ''}`}></span>
             </p>
             <p className='time'>5m fa</p>
           </div>
         </div>
 
-        <div className='post'>
-          <img className='image' src={tres} alt="" />
+        <div className='post' onClick={handlePostClick}>
+        <div className='image3'></div>
           <div>
             <p>
               <span className='name'>Pepita </span>
-              <span>has joined your group </span>
+              <span>si è unito al tuo gruppo </span>
               <span className='group'>2600 Club</span>
-              <span className='status not-read'></span>
+              <span className={`status ${notRead ? 'not-read' : ''}`}></span>
             </p>
             <p className='time'>1 giorno fa</p>
           </div>
         </div>
 
-        <div className='post'>
-          <img className='image' src={cuatro} alt="" />
+        <div className='post' onClick={handlePostClick}>
+        <div className='image4'></div>
           <div>
             <p>
-              <span className='name'>Aracacha </span>
-              <span>sent you a private message </span>
-              <span className='status not-read'></span>
+              <span className='name'>Arakacha </span>
+              <span>ti ha inviato un messaggio privato </span>
+              <span className={`status ${notRead ? 'not-read' : ''}`}></span>
             </p>
             <p className='time'>5 giorni fa</p>
             <div className='message-container'>
@@ -72,12 +83,12 @@ function App() {
           </div>
         </div>
 
-        <div className='post'>
-          <img className='image' src={cinco} alt="" />
+        <div className='post' onClick={handlePostClick}>
+        <div className='image5'></div>
           <div>
             <p>
               <span className='name'>Mazorca </span>
-              <span>commented on your picture </span>
+              <span>ha commentato la tua foto </span>
               <span className='status'></span>
             </p>
             <p className='time'>1 settimana fa</p>
@@ -85,13 +96,13 @@ function App() {
           <img className='picture' src={galaga} alt="" />
         </div>
 
-        <div className='post'>
-          <img className='image' src={seis} alt="" />
+        <div className='post' onClick={handlePostClick}>
+        <div className='image6'></div>
           <div>
             <p>
               <span className='name'>Panela </span>
-              <span>reacted to your recent post </span>
-              <span className='reaction'>5 end-game strategies to increase your win rate</span>
+              <span>ha reagito al tuo post recente </span>
+              <span className='reaction'>5 strategie di fine gioco per aumentare la tua percentuale di vincita</span>
               <span className='status'></span>
             </p>
             <p className='time'>2 settimane fa</p>
@@ -102,7 +113,7 @@ function App() {
 
       </section>
 
-    </body>
+    </>
   )
 }
 
